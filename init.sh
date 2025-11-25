000fab9e7ac60f67dd5629ffca6a4c0a8e24a461
ahnjh05141 <ahnjh05141@naver.com>
2025-11-25 15:33:46 +0900

    feat: init

--- init.sh
+++ init.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
+# 절대경로 기준 설정
+BASE="/home/iot/pi2"
+
 # --------------------------
 # 0) 가상환경 활성화
 # --------------------------
-source venv/bin/activate
+source "$BASE/venv/bin/activate"
 
 # --------------------------
 # 1) motor/move_daemon 실행
 #    - pigpio ON
 # --------------------------
-cd motor
-sudo ./move_daemon &
+cd "$BASE/motor"
+"$BASE/motor/move_daemon" &
 MOVE_PID=$!
 
 # --------------------------
 # 2) sensor/sensor_daemon 실행
 #    - pigpio OFF 모드 강제
-#      (pigpio 충돌 방지)
 # --------------------------
-cd ../sensor
-sudo PIGPIO_NO_INIT=1 ./sensor_daemon 20 &
+cd "$BASE/sensor"
+PIGPIO_NO_INIT=1 "$BASE/sensor/sensor_daemon" 20 &
 SENSOR_PID=$!
 
 # --------------------------
 # 3) 메인 Python 클라이언트 실행
 # --------------------------
-cd ../
-python3 pi2_client.py
+cd "$BASE"
+python3 "$BASE/pi2_client.py"
 
 # --------------------------
 # 4) python 종료되면 데몬들 정리
 # --------------------------
 echo "Stopping daemons..."
-sudo kill $MOVE_PID
-sudo kill $SENSOR_PID
+kill $MOVE_PID
+kill $SENSOR_PID
 
 echo "Done."
